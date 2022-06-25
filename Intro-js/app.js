40a5d8fd86eb2f1375cf2ecd7dd373fea899675a
kenTobias <68740749+kentobias132@users.noreply.github.com>
2022-06-25 14:57:22 +0100

    intro to js task

---
+++ Intro-js/app.js
@@ -0,0 +1,7 @@
+let fullname = 'Kehinde Tobi Joshua';
+let height = '1.5m';
+let country = 'Nigeria';
+
+document.write(`My name is: ${fullname} <br>
+                My height is: ${height} <br>
+                My country is: ${country}`)
